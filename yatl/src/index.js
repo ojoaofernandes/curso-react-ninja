6ee14496b46aa7a36517b4e454243e3f6ac7a382
João Fernandes <ojoaofernandes@gmail.com>
2018-10-02 13:37:39 -0300

    Yatl - Clone react-webpack boilerplate.

---
+++ yatl/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { AppContainer } from 'react-hot-loader';
+import App from './App';
+
+const renderApp = (NextApp) => {
+  render(
+    <AppContainer>
+      <NextApp />
+    </AppContainer>,
+    document.querySelector('[data-js="app"]'),
+  );
+};
+
+renderApp(App);
+
+if (module.hot) {
+  module.hot.accept('./App', () => {
+    const NextApp = require('./App').default;
+    renderApp(NextApp);
+  });
+}
